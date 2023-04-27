a0aab7ccb556ae76e3f913fc30873709df69450c
promo-developer <131697458+promo-developer@users.noreply.github.com>
2023-04-27 15:23:05 -0400

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,17 +1,32 @@
+//you can manually put in percent discounts to entice more customers.
+/////////////////////////////////////////
+//should have a weird function name like: cookie0r87430534u
+function deal(){
 var p=document.createElement("p");
 document.body.appendChild(p);
-var t="10% off your order today!";
+var t="10% off your next purchase!"
 p.innerHTML=t;
 p.style.background="Lime";
 p.style.fontSize="xx-large";
-
-document.cookie = "username=promo; expires=Thu, 18 Dec 3013 12:00:00 UTC; path=/;";
-
+}
+///////////////////////
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+//////////////////////////////////////
 function getCookie(cname) {
   let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -22,7 +37,49 @@ function getCookie(cname) {
   }
   return "";
 }
+///////////////////////////
+function getCookieCheck(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return true;
+    }
+  }
+  return "";
+}
+////////////////////////////
 
-alert(getCookie('promo'));
+function deleteCookie1(){
+document.cookie = "name=10%offLesliesPricingIn1; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
 
-//password is promo123abc
+///////////////////
+
+function ALL15678fghf6rsthrt6(){
+var d=new Date();
+var t=d.getHours();
+
+createCookie("name",t,"0.1");
+
+for(i=0;i<=24;i++){
+
+if(isNaN(parseInt(getCookie(i)))){
+ }
+else{
+if(i>2+i){
+deal();
+   }
+
+ }
+}
+
+}
+
+////////////////////////////
+
+ALL15678fghf6rsthrt6();
